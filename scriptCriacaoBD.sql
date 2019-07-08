82c5b05eee44f2b0d4466f3f539c090deb7da279
JGuerra <joaoguerra97@gmail.com>
2019-07-07 23:03:54 -0300

    Alteração no comportamento das chaves estrangeiras

--- scriptCriacaoBD.sql
+++ scriptCriacaoBD.sql
@@ -11,7 +11,7 @@ CREATE TABLE equipe(
 	id INTEGER NOT NULL,
 	lider_id INTEGER NOT NULL,
 	CONSTRAINT equipe_pk PRIMARY KEY(id),
-	CONSTRAINT equipe_funcionario_fk FOREIGN KEY (lider_id) REFERENCES funcionario (id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT equipe_funcionario_fk FOREIGN KEY (lider_id) REFERENCES funcionario (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS equipes_funcionarios CASCADE;
@@ -21,9 +21,9 @@ CREATE TABLE equipes_funcionarios (
 	CONSTRAINT EQUIPES_FUNCIONARIOS_PK
 	PRIMARY KEY (funcionario_id, equipe_id),
 	CONSTRAINT EQUIPES_FUNCIONARIOS_FUNCIONARIO_FK
-	FOREIGN KEY (funcionario_id) REFERENCES funcionario (id),
+	FOREIGN KEY (funcionario_id) REFERENCES funcionario (id) ON DELETE CASCADE,
 	CONSTRAINT EQUIPES_FUNCIONARIOS_EQUIPE_FK
-	FOREIGN KEY (equipe_id) REFERENCES  equipe (id)
+	FOREIGN KEY (equipe_id) REFERENCES  equipe (id) ON DELETE CASCADE
 );
 	
 DROP TABLE IF EXISTS categoria CASCADE;
@@ -40,5 +40,5 @@ CREATE TABLE projeto(
 	equipe_id INTEGER NOT NULL,
 	CONSTRAINT projeto_pk PRIMARY KEY(id),
 	CONSTRAINT projeto_categoria_fk FOREIGN KEY (categoria_nome) REFERENCES categoria(nome) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT projeto_equipe_fk FOREIGN KEY (equipe_id) REFERENCES equipe(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT projeto_equipe_fk FOREIGN KEY (equipe_id) REFERENCES equipe(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
